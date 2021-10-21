1c7a38977e75cac6ff71acc802d5ceff5fd815ef
HuntingRathalos <huntingandrathalos@gmail.com>
2021-10-21 21:14:44 +0900

    記事詳細

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@
 
 Auth::routes();
 Route::get('/','ArticleController@index' )->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); //-- この行を追加
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth'); //-- この行を追加
+Route::resource('/articles', 'ArticleController')->only(['show']); //-- この行を追加
+
 
